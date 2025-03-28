13a6b4acb96ba100540b2bbfa0db14368d030cf7
V.KARTHIK 192324258 <136962934+karthik7330@users.noreply.github.com>
2025-03-28 01:12:36 -0700

    Update login.py

--- login.py
+++ login.py
@@ -1,4 +1,4 @@
-hdef login(username, password):
+def login(username, password):
     # A simple hardcoded check for demonstration purposes
     if username == "admin" and password == "password":
         return "Login successful"
